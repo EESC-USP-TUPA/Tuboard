a1cb31d72dde8a5fcb2f4010da9945a3b4e10d86
Andrey <andrey.cortez12@gmail.com>
2023-08-17 01:21:06 -0300

    Experimentando com essa bagaça

--- main.py
+++ main.py
@@ -3,18 +3,52 @@ import pandas as pd
 import decode
 import visualize
 
-"""
-TODO: Testar esse código e decidir como essa bagaça vai ser desenvolvida
-"""
-
-
 # Substitua 'nome_do_arquivo' pelo caminho para o arquivo que você deseja ler
 arquivo = '0.bin'
-array_de_bytes = decode.decodificar_arquivo(arquivo)
+dic = decode.decodificar_arquivo(arquivo)
 bytes = decode.file_to_byte_array(arquivo)
-df = pd.DataFrame(array_de_bytes)
 
-print(bytes)
-#print(array_de_bytes)
 
-visualize.plotar_tabela(array_de_bytes)
+"""
+Classe usada para manipular o dado recebido em algo que seja legível e entendível
+
+-> Fazer com que a classe receba o valor direto do arquivo e a partir de uma lambda function 
+processe os dados 
+"""
+class dado:
+    def __init__(self, nome, origem):
+        self.nome = nome
+        self.origem = origem
+        self.dados = []
+        self.tempos = []
+
+    def __getitem__(self, index):
+        return self.dados[index] 
+    
+    def scatter(self):
+        return [(self.tempos[i], self.dados[i]) for i in range(len(self.dados))]
+    
+
+
+apps1_bruto = dado("apps1_bruto", dic)
+apps1_bruto.dados = [(i["dados"][0] << 8) | (i["dados"][1]) for i in dic if i["id"] == 3]
+apps1_bruto.tempos = [(i["tick"]) for i in dic if i["id"] == 3]
+
+apps1_tensao = dado("apps1_tensão", dic)
+apps1_tensao.dados = [float((i["dados"][0] << 8) | (i["dados"][1])) * (3.3/4095.0) for i in dic if i["id"] == 3]
+apps1_tensao.tempos = [(i["tick"]) for i in dic if i["id"] == 3]
+
+apps2_bruto = dado("apps1_bruto", dic)
+apps2_bruto.dados = [(i["dados"][2] << 8) | (i["dados"][3]) for i in dic if i["id"] == 3]
+apps2_bruto.tempos = [(i["tick"]) for i in dic if i["id"] == 3]
+
+apps2_tensao = [float(i)* 3.3/4096.0 for i in apps2_bruto]
+volante_bruto = [(i["dados"][4] << 8) | (i["dados"][5]) for i in dic if i["id"] == 3]
+volante_tensao = [float(i)* 3.3/4096.0 for i in volante_bruto]
+
+
+#print(bytes)
+#print(array_de_bytes)  
+
+#visualize.plotar_tabela([apps1_bruto.tempos, apps1_bruto.dados])
+visualize.plotar_grafico_composto([apps1_bruto, apps2_bruto])
--- visualize.py
+++ visualize.py
@@ -1,11 +1,15 @@
 import numpy as np
 import pyqtgraph as pg
+from pyqtgraph.Qt import QtCore
 
+
+import pyqtgraph.examples
+#pyqtgraph.examples.run()
+
+
+app = pg.mkQApp("Tuboard")
 
 def plotar_tabela(data):
-
-    app = pg.mkQApp("Table Widget Example")
-
     w = pg.TableWidget()
     w.show()
     w.resize(500,500)
@@ -14,6 +18,29 @@ def plotar_tabela(data):
     w.setData(data)
 
     pg.exec()
+
+def plotar_grafico_unico(dado):
+    win = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples")
+    win.resize(1000,600)
+    win.setWindowTitle("Gráfico Único")
+
+    pg.setConfigOptions(antialias=True)
+
+    p1 = win.addPlot(title=dado.nome, y=dado.dados, x=dado.tempos)
+    pg.exec()
+
+def plotar_grafico_composto(dados):
+    win = pg.GraphicsLayoutWidget(show=True, title="Basic plotting examples")
+    win.resize(1000,600)
+    win.setWindowTitle("Gráfico Composto")
+
+    pg.setConfigOptions(antialias=True)
+
+    p1 = win.addPlot(title="hmmmm")
+    p1.multiDataPlot(y = [i.dados for i in dados], x = [i.tempos for i in dados])
+
+    pg.exec()
+
 
 if __name__ == '__main__':
     plotar_tabela()
